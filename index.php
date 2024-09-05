44ac611c39e430e8f207d9ab1f3c93753eaa0f10
Madiken Mustonen <madiken.mustonen@tptlive.ee>
2024-09-05 11:23:21 +0300

    object assignments and simple valiues

--- index.php
+++ index.php
@@ -1,49 +1,35 @@
 <?php
 
-$a = 1;
-$a++;
-$a--;
-$a+=2;
-$a-=2;
-$a*=2;
-$a/=2;
-$a%=2;
+class Box {
+    public $height;
+    public $width;
+    public $lenght;
 
-$a = 'hello' . 'word';
-$a .= 'sjfsljnfslkf';
-$array = array(1,2,3);
-$array = [1,2,3];
-
-var_dump($array[1]);
-$array = [
-    'essa' => 1,
-    'tessa' => 2,
-    'kossa' => 3,
-    5 => 4,
-    'chad',
-    1.2,
-    true,
-
-];
-var_dump($array['kossa']);
-
-var_dump($array);
-//foreach($array as $key => $valiue){
-//    echo $key . '=>' .$valiue. '\in';
-//}
-
-var_dump($array);
-foreach($array as $key => $valiue){
-    //echo $key . '=>' .$valiue. '\in';
+    public function volume(){
+        return $this->height * $this->width * $this->lenght;
+    } 
 }
-$name = 'madiken';
 
-$string = "hello $name\n";
-var_dump($string);
+class Metalbox extends Box {
+    public $weightPerUnit = 10;
+    public function weight(){
+        return $this->volume() * $this->wheightPerUnit;
+    }
 
-$char = 'a';
-
-for($i=0;$i<100;$i++){
-    var_dump($char);
-    $char++;
 }
+
+$num1 = 1;
+$num2 = $num1;
+$num2 = 2;
+var_dump($num1, $num2);
+
+
+
+
+
+$metal1 = new Metalbox();
+$metal1->height = 1;
+$metal2 = $metal2;
+$metal2->height = 2;
+var_dump($metal1, $metal2);
+
