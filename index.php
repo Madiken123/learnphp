4b43f7e4e9619ad1097847f1cae2c7ea2daca212
Madiken Mustonen <madiken.mustonen@tptlive.ee>
2024-09-06 14:49:48 +0300

    static

--- index.php
+++ index.php
@@ -1,41 +1,22 @@
 <?php
 
 class Box {
+    public static $count = 0;
+    public $id;
     public function __construct(){
-        echo "box was created\n";
+        $this->id =self::$count++;
     }
-    public function __call($name, $args){
-        var_dump($name, $args);
-    }
-    public function __get($name){
-        return 'cool value';
-    }
-    public function __set($name, $value){
-        var_dump($name, $value);
-    }
-    public function __destruct(){
-        echo "box was destroyed\n";
-    }
-    public function __toString(){
-        return 'cool box';
-    }
-    public function __invoke(...$args){
-        var_dump($args);
-    }
-}
+    public static function setCount(){
+       // var_dump($this->id);
+        var_dump(self::$count);
 
-function hello(){
-    $box =new Box();
-}
+    } 
+    public static function name(){
+        var_dump(static::class);
+    }
+    }
+class Metalbox extends box{
 
-$box = new Box();
-$box->name = 'Madiken';
-$box->something(1,2,'asd');
-$box->else('xd', true, NULL);
-var_dump($box->cats);
-var_dump($box);
-echo $box;
-$box(1,'asd', 'asda');
+} 
 
-
-?>
+    var_dump(box::class);
