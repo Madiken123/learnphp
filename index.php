abb3832bba7badf5b38156fe47dc79be587e581f
Madiken Mustonen <madiken.mustonen@tptlive.ee>
2024-09-06 13:23:15 +0300

    intrrface vark

--- index.php
+++ index.php
@@ -1,42 +1,39 @@
 <?php
 
-class Box {
-    public $height;
-    public $width;
-    public $lenght;
-
-    public function volume(){
-        return $this->height * $this->width * $this->lenght;
-    } 
-}
-
-class Metalbox extends Box {
-    public $weightPerUnit = 10;
-    public function weight(){
-        return $this->volume() * $this->wheightPerUnit;
-    }
-
-}
-
-$num1 = 1;
-$num2 = $num1;
-$num2 = 2;
-var_dump($num1, $num2);
-
-
-
-
-
-$metal1 = new Metalbox();
-$metal1->height = 1;
-$metal2 = $metal2;
-$metal2->height = 2;
-var_dump($metal1, $metal2);
-
-trait HasMaterial {
-    public $metal;
-    public function getMaterial() {
-        return $this->material;
+class Consolelogger {
+    public function log($message){
+        echo "$message\n";
     }
 }
 
+class Task {
+    public function work(consolelogger $logger){
+        for($i=0;$i<10;$i++){
+            $logger->log($i);
+        }
+    }
+}
+
+interface logger {
+    public function log($message);
+}
+
+//-----------------------------------
+
+class Nothinglogger{
+    public function log($message){
+
+    }
+}
+
+class Filelogger {
+    public function log($message){
+        $file = fopen('log.txt', 'a');
+        fwrite($file, "$message\n");
+        fclose($file);
+    }
+}
+
+$logger = new Filelogger;
+$task = new task();
+$task->work($logger);
