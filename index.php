8977f5a279c54cbc0c12001a35bc6cb4129239d4
Madiken Mustonen <madiken.mustonen@tptlive.ee>
2024-09-06 13:41:43 +0300

    visibility

--- index.php
+++ index.php
@@ -1,39 +1,38 @@
 <?php
 
-class Consolelogger {
-    public function log($message){
-        echo "$message\n";
-    }
-}
+class Box {
+    public $height;
+    protected $width;
+    private $lenght;
 
-class Task {
-    public function work(consolelogger $logger){
-        for($i=0;$i<10;$i++){
-            $logger->log($i);
+    public function volume(){
+        return $this->height * $this->width * $this->lenght;
+    } 
+    public function getLenght(){
+        return $this->lenght;
+    }
+    public function setLenght(){
+        if(is_numeric($lenght) && $lenght > 0 ) {
+            $this->lenght = $lenght;
+        } else {
+            $this->lenght = 0;
         }
     }
 }
 
-interface logger {
-    public function log($message);
-}
-
-//-----------------------------------
-
-class Nothinglogger{
-    public function log($message){
-
+class Metalbox extends Box {
+    public $weightPerUnit = 10;
+    public function weight(){
+        return $this->volume() * $this->wheightPerUnit;
+    }
+    public function testProtected(){
+        var_dump($this->lenght);
     }
 }
 
-class Filelogger {
-    public function log($message){
-        $file = fopen('log.txt', 'a');
-        fwrite($file, "$message\n");
-        fclose($file);
-    }
-}
+$box = new metalbox();
+$box->setlenght(121);
+$box->testProtected(141);
+var_dump($box); 
 
-$logger = new Filelogger;
-$task = new task();
-$task->work($logger);
+?>
