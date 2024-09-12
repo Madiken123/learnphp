6d2b814e9a02dfc54e3cb7ba2c6b8790f04f2b94
Madiken Mustonen <madiken.mustonen@tptlive.ee>
2024-09-12 11:26:28 +0300

    see katki

--- public/index.php
+++ public/index.php
@@ -7,54 +7,49 @@ if (preg_match('/\.(?:png|jpg|jpeg|gif|svg|js|css)$/', $_SERVER["REQUEST_URI"]))
 
 function dump(...$vars){
 echo '<pre>';
-var_dump($_SERVER);
+var_dump($_vars);
 echo '</pre>';
 }
 
 
 spl_autoload_register(function($class){
     $class = substr($class,4);
- dump($class);
  require_once "sc/$class.php";
 });
 
-// require_once 'src/router2.php';
-// require_once 'src/DB.php.php';
+use App\Router;
 
+Router::addRoute('/', function(){
+    $posts=[
+        ['title' => 'some  1', 'body' => 'some body 1'],
+        ['title' => 'some  4', 'body' => 'some body 4'], 
+        ['title' => 'some  2', 'body' => 'some body 2'],
+    ];
+        include 'views/index.php';
+});
 
-$router = new App\router();
-$db = new App\DB();
-$Controller = new App\Controllers\PublicController();
-dump($router, $db, $Controller);
+Router::addRoute('/us', function(){
+    $posts=[
+        ['title' => 'some  1', 'body' => 'some body 1'],
+        ['title' => 'some  4', 'body' => 'some body 4'], 
+        ['title' => 'some  2', 'body' => 'some body 2'],
+    ];
+        include 'views/us.php';
+});
 
-// switch($_SERVER['REQUEST_URI']){
-//     case '/':
-//         $posts = [
-//             ['title' => 'some  1', 'body' => 'some body 1'],
-//             ['title' => 'some  2', 'body' => 'some body 2'],
-//             ['title' => 'some  3', 'body' => 'some body 3'],
-//             ['title' => 'some  4', 'body' => 'some body 4'],   
-//         ];
-//         include 'views/index.php';
-//         break;
-//         case '/us':
-//             $posts = [
-//                 ['title' => 'some us 1', 'body' => 'some body 1'],
-//                 ['title' => 'some US 2', 'body' => 'some body 2'],
-//                 ['title' => 'some US 3', 'body' => 'some body 3'],
-//                 ['title' => 'some US 4', 'body' => 'some body 4'],   
-//             ];
-//         include 'views/us.php';
-//         break;
-//         case '/tech':
-//             $posts = [
-//                 ['title' => 'some tech 1', 'body' => 'some body 1'],
-//                 ['title' => 'some tech 2', 'body' => 'some body 2'],
-//                 ['title' => 'some tech 3', 'body' => 'some body 3'],
-//                 ['title' => 'some vtech 4', 'body' => 'some body 4'],   
-//             ];
-//         include 'views/tech.php';
-//         break;
-//         default:
-//         echo 'ERROR 404';
-// }
+Router::addRoute('/tech', function(){
+    $posts=[
+        ['title' => 'some tech  1', 'body' => 'some body 1'],
+        ['title' => 'some  4', 'body' => 'some body 4'], 
+        ['title' => 'some  2', 'body' => 'some body 2'],
+    ];
+        include 'views/tech.php';
+});
+$router = new Router($_SERVER['REQUEST_URI']);
+$match = $router->match();
+if ($match){
+    call_user_func($match['action']);
+} else {
+    echo 'ERROR 404';
+
+}
